edf1ef080b2f710fa0afa431c0172898d6e283d0
OXOYO <zmn2007.hi@163.com>
2018-02-12 16:05:11 +0800

    创建heroku_v2.0.0分支，分支代码来自marter打包。

--- src/platform/apps/Admin/store/state.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by OXOYO on 2017/12/5.
- */
-
-export default {
-  appData: {},
-  _appData: {}
-}
--- src/store/platform/state.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by OXOYO on 2017/12/5.
- */
-
-export default {
-  userInfo: {
-    isLogin: false
-  }
-}
